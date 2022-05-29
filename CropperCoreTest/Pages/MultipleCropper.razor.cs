a1495a2d93c01ac3c0dd2e62515cf5b354af6d58
Ivano Bandini <ivano.bandini@hotmail.it>
2022-05-29 09:52:48 +0200

    Removed css and unused icons. Changed the home page and removed unused elements. Multi crop example added.

---
+++ CropperCoreTest/Pages/MultipleCropper.razor.cs
@@ -0,0 +1,73 @@
+﻿using CropperCore.Classes;
+using CropperCore.Classes.Events;
+using CropperCore.Enumerates;
+using CropperCore.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
+
+namespace CropperCoreTest.Pages
+{
+    public partial class MultipleCropper
+    {
+        [Inject]
+        private CropperService CropperService { get; set; } = null!;
+
+        [Inject]
+        private IJSRuntime JS { get; set; } = null!;
+
+        private ElementReference imgRef1;
+        private ElementReference imgRef2;
+
+        private string imgUrl = null!;
+
+       protected override void OnInitialized()
+        {
+            imgUrl = "bridge.jpg";
+
+            base.OnInitialized();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await CreateCrop();
+
+                await JS.InvokeVoidAsync("addTooltips");
+
+                StateHasChanged();
+            }
+
+            base.OnAfterRender(firstRender);
+        }
+
+        private CropperOtions Options { get; set; } = new CropperOtions
+        {
+            //AutoCrop = true,
+            //ViewMode = ViewMode.RestrictCrop
+        };
+
+        private async Task CreateCrop()
+        {
+            try
+            {
+                await CropperService.Create(imgRef1, Options, "image1");
+                await CropperService.Create(imgRef2, Options, "image2");
+            }
+            catch (Exception ex)
+            {
+                //Error = ex.Message;
+            }
+        }
+
+        //public async ValueTask DisposeAsync()
+        //{
+        //    await CropperService?.DisposeAsync();
+        //}
+    }
+}
